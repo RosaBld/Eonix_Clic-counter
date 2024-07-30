6d274e578088aacb1cb0094bef852d384ed7f9d8
Rosa <rosalie.boulard@gmail.com>
2024-07-31 00:16:56 +0200

    fixing the error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// makes redux store available in the entire app - manage app's state
+import configureStore from './configureStore';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+
+const initialState = {};
+const history = createBrowserHistory();
+const store = configureStore(initialState, history);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducers.js
+++ src/reducers.js
@@ -6,6 +6,14 @@ import { combineReducers } from 'redux';
 // import languageProviderReducer from './containers/LanguageProvider/reducer';
 // import globalReducer from './containers/App/reducer';
 
+// adding a sample reducer to ensure the combineReduces receives one valide reduces
+const sampleReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
@@ -13,6 +21,8 @@ export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
     // language: languageProviderReducer,
     // global: globalReducer,
+    //conflict with language and global reducers?
+    sample: sampleReducer,
     ...injectedReducers,
   });
 
